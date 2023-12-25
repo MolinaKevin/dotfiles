96b70c3718aa49ffa907b4540e4e4a2e2e7dbde4
Molina Kevin <molinakevinalejandro@gmail.com>
2023-12-25 23:13:07 +0100

    added dependencies in install

--- install.sh
+++ install.sh
@@ -2,15 +2,15 @@
 
 # Función para mostrar el menú
 mostrar_menu() {
-    clear
-    echo "Instalación de dotfiles de Kevin Molina:"
-    echo "1. Instalar comandos RUST (Necesarios para Fish principalmente)"
-    echo "2. Instalar fish + starship"
-    echo "3. Paru"
-    echo "4. Instalar xmonad + xmobar y agregados"
-    echo "5. Ejecutar Función 2"
-    echo "6. Salir"
-    echo
+	clear
+	echo "Instalación de dotfiles de Kevin Molina:"
+	echo "1. Instalar comandos RUST (Necesarios para Fish principalmente)"
+	echo "2. Instalar fish + starship"
+	echo "3. Paru (Necesario para instalar xmonad)"
+	echo "4. Instalar xmonad + xmobar y agregados"
+	echo "5. Ejecutar Función 2"
+	echo "6. Salir"
+	echo
 }
 
 funcion_1() {
@@ -53,33 +53,44 @@ funcion_3() {
 
 funcion_4() {
 	echo "Instalando dependencias que uso para xmonad"
-	sudo pacman -S git lxsession picom --noconfirm --needed
+	sudo pacman -S git lxsession picom python-pywal yq xdotool --noconfirm --needed
+
+
 	echo "Instalando stack y ghc"
 	curl -sSL https://get.haskellstack.org/ | sh
+	sudo pacman -S ghc --noconfirm --needed
 	
 	echo "Instalando xmonad xmobar y contrib"
 	cd $HOME/.config/xmonad
 	stack init
+	stack install
 
 	mkdir $HOME/.config/xmobar
 	stack init
+	stack install
 	
 	echo "Instalando conky"
 	paru -S conky-lua --noconfirm --needed
+
+	echo "Instalando rofi"
+	paru -S rofi --noconfirm --needed
+
+	echo "Instalando extras"
+	paru -S feh --noconfirm --needed
 }
 
 # Loop principal del menú
 while true; do
-    mostrar_menu
-    read -p "Por favor, elige una opción: " opcion
-    case $opcion in
-        1) funcion_1 ;;
-        2) funcion_2 ;;
-        3) funcion_3 ;;
-        4) funcion_4 ;;
-        5) echo "Saliendo..." ; exit ;;
-        *) echo "Opción inválida. Introduce una opción válida." ;;
-    esac
-    read -p "Presiona Enter para continuar..."
+	mostrar_menu
+	read -p "Por favor, elige una opción: " opcion
+	case $opcion in
+		1) funcion_1 ;;
+		2) funcion_2 ;;
+		3) funcion_3 ;;
+		4) funcion_4 ;;
+		5) echo "Saliendo..." ; exit ;;
+		*) echo "Opción inválida. Introduce una opción válida." ;;
+	esac
+	read -p "Presiona Enter para continuar..."
 done
 
